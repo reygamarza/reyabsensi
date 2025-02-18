a55eba77bc91ec6be5ef01b7064a07c8388e35ce
reygamarza <marzareyga@gmail.com>
2025-02-18 15:10:41 +0700

    fix: layouts for all roles and dynamic navbar

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,40 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>@yield('title', 'MyApp')</title>
+
+<!-- Fontfaces CSS-->
+<link href="{{ asset('assets/kesiswaan') }}/css/font-face.css" rel="stylesheet" media="all">
+<link href="{{ asset('assets/kesiswaan') }}/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet"
+    media="all">
+<link href="{{ asset('assets/kesiswaan') }}/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet"
+    media="all">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link href="{{ asset('assets/kesiswaan') }}/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet"
+    media="all">
+
+<!-- Bootstrap CSS-->
+<link href="{{ asset('assets/kesiswaan') }}/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+
+
+<!-- Vendor CSS-->
+<link href="{{ asset('assets/kesiswaan') }}/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+<link href="{{ asset('assets/kesiswaan') }}/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css"
+    rel="stylesheet" media="all">
+<link href="{{ asset('assets/kesiswaan') }}/vendor/wow/animate.css" rel="stylesheet" media="all">
+<link href="{{ asset('assets/kesiswaan') }}/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+<link href="{{ asset('assets/kesiswaan') }}/vendor/slick/slick.css" rel="stylesheet" media="all">
+<link href="{{ asset('assets/kesiswaan') }}/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+<link href="{{ asset('assets/kesiswaan') }}/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet"
+    media="all">
+
+<!-- Main CSS-->
+<link href="{{ asset('assets/kesiswaan') }}/css/theme.css" rel="stylesheet" media="all">
+<link rel="shortcut icon" href="{{ asset('assets/kesiswaan') }}/images/icon/iconabas.png">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,58 @@
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-bottom-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+
+        @if (Session::has('berhasil'))
+            toastr.success("{{ Session::get('berhasil') }}");
+        @endif
+
+        @if (Session::has('gagal'))
+            toastr.error("{{ Session::get('gagal') }}");
+        @endif
+    });
+</script>
+
+<!-- Jquery JS-->
+<script src="{{ asset('assets/kesiswaan') }}/vendor/jquery-3.2.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Bootstrap JS-->
+<script src="{{ asset('assets/kesiswaan') }}/vendor/bootstrap-4.1/popper.min.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+<!-- Vendor JS       -->
+<script src="{{ asset('assets/kesiswaan') }}/vendor/slick/slick.min.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/wow/wow.min.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/animsition/animsition.min.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/counter-up/jquery.waypoints.min.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/counter-up/jquery.counterup.min.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/circle-progress/circle-progress.min.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/chartjs/Chart.bundle.min.js"></script>
+<script src="{{ asset('assets/kesiswaan') }}/vendor/select2/select2.min.js"></script>
+
+<!-- Main JS-->
+<script src="{{ asset('assets/kesiswaan') }}/js/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+    integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+@stack('myscript')
